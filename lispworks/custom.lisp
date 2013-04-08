b7eaad2007e307422e1485ea39465b440218c79e
James M. Lawrence <llmjjmll@gmail.com>
2013-04-08 19:21:50 -0400

    add support for old LispWorks struct format

--- lispworks/custom.lisp
+++ lispworks/custom.lisp
@@ -24,7 +24,44 @@
       (incf count))
     (store-object count stream)))
 
+(defun lw-old-structure-object-restore (new-instance length stream)
+  ;; old format -- length is stored first
+  (declare (optimize speed))
+  (resolving-object (obj new-instance)
+    (loop repeat length do
+         (let ((slot-name (restore-object stream)))
+           ;; slot-names are always symbols so we don't
+           ;; have to worry about circularities
+           (setting (slot-value obj slot-name) (restore-object stream))))))
+
+(defun lw-normal-structure-object-restore (new-instance first-slot-name stream)
+  ;; portable format -- length is stored at the end
+  (declare (optimize speed))
+  (resolving-object (obj new-instance)
+    (setting (slot-value obj first-slot-name) (restore-object stream))
+    (loop for count from 1 do
+         (let ((slot-name (restore-object stream)))
+           (etypecase slot-name
+             (integer (assert (= count slot-name) (count slot-name)
+                       "Number of slots restored does not match slots stored.")
+                      (return))
+             (symbol 
+              ;; slot-names are always symbols so we don't
+              ;; have to worry about circularities
+              (setting (slot-value obj slot-name) (restore-object stream))))))))
+
 (defrestore-cl-store (structure-object stream)
-  (restore-type-object stream))
+  (declare (optimize speed))
+  (let* ((class (find-class (restore-object stream)))
+         (new-instance (allocate-instance class))
+         (slot-name-or-length (restore-object stream)))
+    (etypecase slot-name-or-length
+      (integer (unless (zerop slot-name-or-length)
+                 (warn "Reading obsolete LispWorks structure-object format.")
+                 (lw-old-structure-object-restore
+                  new-instance slot-name-or-length stream)))
+      (symbol (lw-normal-structure-object-restore
+               new-instance slot-name-or-length stream)))
+    new-instance))
 
 ;; EOF
